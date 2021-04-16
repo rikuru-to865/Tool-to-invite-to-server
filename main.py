1e3a25b92ee247dfff4fb5fc19bd78262a381987
rikuru-to865 <usaginosippo865@gmail.com>
2021-04-16 22:15:20 +0900

    added input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+invitecode = input("招待urlを入力")
 tokens = []
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
 with open("./varitify_token.txt",mode="r") as f:
@@ -12,6 +12,6 @@ with open("./proxys.txt",mode="r") as f:
         proxyDict = { 
               "http"  : i
             }
-        r = requests.post("https://discord.com/api/v8/invites/5NUS4ndz",proxies=proxyDict,headers={'authorization':tokens[b].replace("\n",""),'User-Agent': ua})
+        r = requests.post("https://discord.com/api/v8/invites/"+invitecode,proxies=proxyDict,headers={'authorization':tokens[b].replace("\n",""),'User-Agent': ua})
         print(int(r.status_code))
         b+=1
